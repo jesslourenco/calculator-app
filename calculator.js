03c53cd1533789e5a3553effe8cf13139e796d9e
Jess <jrizzo.br@gmail.com>
2021-06-17 12:53:46 -0500

    Refactored code so handleButtonClick() is cleaner and easier to read Attention: Bugs were not corrected if existed in main. Only code design was worked on

--- calculator.js
+++ calculator.js
@@ -2,8 +2,11 @@ let operator;
 let operatorState;
 let num1;
 let num2;
+//let digitsOnScreen = document.getElementById("screen").innerHTML;
+let btnDecSeparator = document.getElementById("decimalSeparator");
 
-// CSS Event Functions
+
+// CSS Event Functions for Theme Selection
 function themeSelection(){
     let selection;
     const options = document.getElementsByName('themes');
@@ -64,67 +67,103 @@ function classicTheme(){
 function handleButtonClick(value){
     console.log(value);
 
-    const operators = ['x', '/', '+', '-'];
-    let digitsOnScreen = document.getElementById('screen').innerHTML;
-    
-    if (Number.isInteger(parseInt(value)) === true){
-        if (operatorState){
-            operatorState = false;
-            document.getElementById("decimalSeparator").classList.remove('dot-disabled');
-            clearScreen();
-        }
-        
-        showOnScreen(value);
-    }
+    let valueType = getTypeOfValue(value);     
 
-    if (operators.includes(value)){
-        if (!operator){
-            num1 = digitsOnScreen;
-        } else if(num1) {
-            num2 = digitsOnScreen
-        } else {
-            const result = calculateResult(operator, num1, num2);          
-            num1 = String(result);
-            num2 = undefined;  
-        }
-
-        operator = value;
-        operatorState = true; 
-    }    
-  
-    switch(value){ 
+    switch(valueType){ 
         case 'NUMBER':
+            handlesNumber(value);
             return;
         case 'OPERATOR':
+            handlesOperator(value);
             return;
         case 'RESET':
-            clearScreen();
-            operator = num1 = num2 = undefined;
+            handlesReset();
             return;
         case 'DEL':
-            deletevalue();
+            handlesDelete();
             return;
         case '=':
-            num2 = document.getElementById('screen').innerHTML;
-            result = calculateResult(operator, num1, num2);
-            num1 = num2 = operator = undefined;
-            operatorState = false; 
-            document.getElementById("decimalSeparator").classList.remove('dot-disabled');  
+            handlesEquals();
             return;
         case '.':
-            showOnScreen(value);
-            document.getElementById("decimalSeparator").className += ' dot-disabled';
+            handlesDecSeparator(value);
             return;
-    }     
+    }  
+    
+    return console.log("Something went wrong");
+}
 
-    return console.log(value);
+function getTypeOfValue(value){
+    const operators = ['x', '/', '+', '-'];
+
+    if (Number.isInteger(parseInt(value)) === true){
+        return 'NUMBER'
+    } else if (operators.includes(value)){
+        return 'OPERATOR'
+    } else {
+        return value;
+    }
+}
+
+function handlesNumber(value){
+    if (operatorState){
+        operatorState = false;
+        btnDecSeparator.classList.remove('dot-disabled');
+        clearScreen();
+    }        
+    showOnScreen(value);
+}
+
+function handlesOperator(value){
+
+    if (!operator){
+        num1 = document.getElementById("screen").innerHTML;
+    }else if(num1) {
+        num2 = document.getElementById("screen").innerHTML;
+    }
+    if (operator && num1 && num2){
+        const result = calculateResult(operator, num1, num2);          
+        num1 = String(result);
+        num2 = undefined;  
+    }
+
+    operator = value;
+    operatorState = true; 
+}
+
+function handlesReset(){
+    clearScreen();
+    operator = num1 = num2 = undefined;
+}
+
+function handlesDelete(){
+    let digits = document.getElementById('screen').innerHTML;
+    if (digits[digits.length-1] === '.'){
+        document.getElementById("decimalSeparator").classList.remove('dot-disabled');    
+    }
+    digits = digits.slice(0,-1);
+    clearScreen();
+    showOnScreen(digits);
+}
+
+function handlesEquals(){
+    num2 = document.getElementById('screen').innerHTML;
+    result = calculateResult(operator, num1, num2);
+    num1 = num2 = operator = undefined;
+    operatorState = false; 
+    btnDecSeparator.classList.remove('dot-disabled');
+}
+
+function handlesDecSeparator(value){
+    showOnScreen(value);
+    btnDecSeparator.className += ' dot-disabled';
 }
 
 function showOnScreen(value){
     document.getElementById('screen').innerHTML += value;
 }
 
-function calculateResult(operator, savedNum, screenNum){
+function calculateResult(operator, num1, num2){
     if(operator === '/' && parseInt(screenNum) === 0){
         clearScreen();
         let text = 'Infinity!';
@@ -133,22 +172,22 @@ function calculateResult(operator, savedNum, screenNum){
         return;
     }
 
-    let num1 = parseToNum(savedNum);
-    let num2 = parseToNum(screenNum);
+    let first = parseToNum(num1);
+    let second = parseToNum(num2);
     let result = 0;
 
     switch(operator){
         case '/':
-            result = divide(num1,num2);
+            result = divide(first,second);
             break;
         case 'x':
-            result = multiply(num1,num2);
+            result = multiply(first,second);
             break;
         case '+':
-            result = sum(num1,num2);
+            result = sum(first,second);
             break;
         case '-':
-            result = subtract(num1,num2);
+            result = subtract(first,second);
             break;
     }
     if (result.toString().length > 10){
@@ -156,6 +195,7 @@ function calculateResult(operator, savedNum, screenNum){
         result = result.toFixed(9);
     }
     document.getElementById('screen').innerHTML = result;
+    console.log(result);
     return result;
 }
 
@@ -193,15 +233,3 @@ function clearScreen(){
     }
     document.getElementById('screen').innerHTML = '';
 }
-
-function deletevalue(){
-    let digits = document.getElementById('screen').innerHTML;
-    if (digits[digits.length-1] === '.'){
-        document.getElementById("decimalSeparator").classList.remove('dot-disabled');    
-    }
-    digits = digits.slice(0,-1);
-    clearScreen();
-    showOnScreen(digits);
-}
-
-
